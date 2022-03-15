963ed23bf7c04088855ac8498742fd885726c82c
Kristján Oddsson <koddsson@gmail.com>
2022-03-15 13:09:53 +0000

    Add a test for when the `<summary>` is in a weird place

--- test/linters/accessibility/details_has_summary_test.rb
+++ test/linters/accessibility/details_has_summary_test.rb
@@ -14,6 +14,13 @@ class DetailsHasSummary < LinterTestCase
     assert_equal @linter.offenses.count, 1
   end
 
+  def test_does_not_warn_if_details_has_a_summary_in_a_weird_place
+    @file = "<details><p>Surprise text!</p><summary>Expand me!</summary></details>"
+    @linter.run(processed_source)
+
+    assert_empty @linter.offenses
+  end
+  
   def test_does_not_warn_if_details_has_a_summary
     @file = "<details><summary>Expand me!</summary><button>Surprise button!</button></details>"
     @linter.run(processed_source)
