3480742750224651b14c3e2be7c47ac255e99048
Kate Higa <khiga8@github.com>
2022-06-08 22:50:43 -0500

    Introduce generic link text linter

---
+++ docs/rules/accessibility/avoid-generic-link-text-counter.md
@@ -0,0 +1,47 @@
+# Avoid generic link text
+
+## Rule Details
+
+Avoid setting generic link text like, "Click here", "Read more", and "Learn more" which do not make sense when read out of context. Screen reader users often tab through links on a page to quickly find content without needing to listen to the full page. When link text is too generic, it becomes difficult to quickly identify the destination of the link.
+
+## Resources
+
+- [Primer: Links](https://primer.style/design/accessibility/link)
+- [Understanding Success Criterion 2.4.4: Link Purpose (In Context)](https://www.w3.org/WAI/WCAG21/Understanding/link-purpose-in-context.html)
+- [WebAim: Links and Hypertext](https://webaim.org/techniques/hypertext/)
+- [Deque: Use link text that make sense when read out of context](https://dequeuniversity.com/tips/link-text)
+
+## Examples
+
+### **Incorrect** code for this rule 👎
+
+```erb
+<a href="github.com/about">Learn more</a>
+```
+
+```erb
+<!-- also bad -->
+<a href="github.com/about">Read more</a>
+```
+
+```erb
+<!-- also bad -->
+<span>
+  <a href="github.com/new">Click here</a> to create a new repository.
+</span>
+```
+
+```erb
+<!-- also bad -->
+<%= link_to "Learn more", "#" %>
+```
+
+### **Correct** code for this rule  👍
+
+```erb
+<a href="github.com/about">Learn more about GitHub</a>
+```
+
+```erb
+<a href="github.com/new">Create a new repository</a>
+```
---
+++ lib/erblint-github/linters/github/accessibility/avoid_generic_link_text_counter.rb
@@ -0,0 +1,100 @@
+# frozen_string_literal: true
+
+require_relative "../../custom_helpers"
+
+module ERBLint
+  module Linters
+    module GitHub
+      module Accessibility
+        class AvoidGenericLinkTextCounter < Linter
+          include ERBLint::Linters::CustomHelpers
+          include LinterRegistry
+
+          BANNED_GENERIC_TEXT = [
+            "Read more",
+            "Learn more",
+            "Click here",
+            "More",
+            "Link"
+          ].freeze
+          MESSAGE = "Avoid using generic link text such as #{BANNED_GENERIC_TEXT.join(', ')} which do not make sense in isolation."
+
+          def run(processed_source)
+            processed_source.ast.children.each_with_index do |node, index|
+              next unless node.methods.include?(:type) && node.type == :text
+              text = node.children.join.strip
+              # Checks HTML tags
+              if banned_text?(text)
+                prev_node = processed_source.ast.children[index-1]
+                next_node = processed_source.ast.children[index+1]
+    
+                next unless tag?(prev_node) && tag?(next_node)
+    
+                prev_node_tag = BetterHtml::Tree::Tag.from_node(prev_node)
+                next_node_tag = BetterHtml::Tree::Tag.from_node(next_node)
+
+                # We only report if the text is nested between two link tags.
+                if link_tag?(prev_node_tag) && link_tag?(next_node_tag) && next_node_tag.closing?
+                  generate_offense(self.class, processed_source, prev_node_tag)
+                end
+              end
+    
+              # Checks Rails link helpers like `link_to`
+              erb_node = node.type == :erb ? node : node.descendants(:erb).first
+              next unless erb_node
+              _, _, code_node = *erb_node
+              source = code_node.loc.source
+              ruby_node = extract_ruby_node(source)
+              send_node = ruby_node&.descendants(:send)&.first
+              if send_node.methods.include?(:method_name) && send_node.method_name == :link_to
+                send_node.child_nodes.each do |child_node|
+                  if child_node.methods.include?(:type) && child_node.type == :str
+                    if banned_text?(child_node.children.join)
+                      tag = BetterHtml::Tree::Tag.from_node(code_node)
+                      generate_offense(self.class, processed_source, tag)
+                    end
+                  end
+                end
+              end
+            end
+            counter_correct?(processed_source)
+          end
+
+          def autocorrect(processed_source, offense)
+            return unless offense.context
+
+            lambda do |corrector|
+              if processed_source.file_content.include?("erblint:counter #{simple_class_name}")
+                # update the counter if exists
+                corrector.replace(offense.source_range, offense.context)
+              else
+                # add comment with counter if none
+                corrector.insert_before(processed_source.source_buffer.source_range, "#{offense.context}\n")
+              end
+            end
+          end
+
+          private
+
+          def banned_text?(text)
+            BANNED_GENERIC_TEXT.map(&:downcase).include?(text.downcase)
+          end
+    
+          def extract_ruby_node(source)
+            BetterHtml::TestHelper::RubyNode.parse(source)
+          rescue ::Parser::SyntaxError
+            nil
+          end
+
+          def link_tag?(tag_node)
+            tag_node.name == "a"
+          end
+
+          def tag?(node)
+            node.methods.include?(:type) && node.type == :tag
+          end
+        end
+      end
+    end
+  end
+end
